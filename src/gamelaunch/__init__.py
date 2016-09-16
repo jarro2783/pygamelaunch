d9580c67d08a946353c4b3b689aa3c34052898d8
Jarryd Beck <jarro.2783@gmail.com>
2016-09-16 19:02:38 +1000

    add some pyterm

--- src/gamelaunch/__init__.py
+++ src/gamelaunch/__init__.py
@@ -1,5 +1,17 @@
 import pyterm
 
 class Game:
-    def __init__(self):
-        self.__capture = pyterm.Capture
+    def __init__(self,
+                 program,
+                 arguments,
+                 record_host,
+                 record_port,
+                 record_user):
+        self.__program = pyterm.ExecProgram("program", *arguments)
+        net_writer = pyterm.ExecWriter("termrecord_client",
+            "-host", record_host, "-port", record_port,
+            "-user", record_user, "-send")
+        self.__capture = pyterm.Capture(program, writers=[net_writer])
+
+    def run(self):
+        self.__capture.run()
