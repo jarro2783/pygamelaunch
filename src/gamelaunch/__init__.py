0ac15c31c49b673311bbfc4e19203cc7e1d18932
Jarryd Beck <jarro.2783@gmail.com>
2016-09-19 08:01:47 +1000

    improve recording code

--- src/gamelaunch/__init__.py
+++ src/gamelaunch/__init__.py
@@ -7,11 +7,35 @@ class Game:
                  record_host,
                  record_port,
                  record_user):
-        self.__program = pyterm.ExecProgram(program, *arguments)
-        net_writer = pyterm.ExecWriter("termrecord_client",
-            "-host", record_host, "-port", record_port,
-            "-user", record_user, "-send")
-        self.__capture = pyterm.Capture(program, writers=[net_writer])
+        self.__program = program
+        self.__args = arguments
+        self.__record_host = record_host
+        self.__record_port = record_port
+        self.__record_user = record_user
 
     def run(self):
-        self.__capture.run()
+        executor = pyterm.ExecProgram(self.__program, self.__args)
+        net_writer = pyterm.ExecWriter("termrecord_client",
+            "-host", self.__record_host, "-port", self.__record_port,
+            "-user", self.__record_user, "-send")
+        capture = pyterm.Capture(program, writers=[net_writer])
+        capture.run()
+
+class Watcher:
+    def __init__(self, server, port, watch_user):
+        self.__server = server
+        self.__port = port
+        self.__user = watch_user
+        self.__watcher = pyterm.ExecWatcher("termrecord_client",
+            [
+                "-host",
+                server,
+                "-port",
+                port,
+                "-user",
+                watch_user,
+                "-watch"
+            ])
+
+    def watch(self):
+        self.__watcher.watch()
