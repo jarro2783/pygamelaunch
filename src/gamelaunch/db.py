c69f8c6ec4ef7adcbd537eec36b0c1dfbe13a447
Jarryd Beck <jarro.2783@gmail.com>
2016-10-20 08:31:57 +1100

    try to fix encoding

--- src/gamelaunch/db.py
+++ src/gamelaunch/db.py
@@ -36,7 +36,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, index=True)
-    password = Column(String)
+    password = Column(String(convert_unicode=True))
     salt = Column(String)
     email = Column(String)
 
