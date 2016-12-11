4fc2957074e10cfd99620976a83c4fb1e466286f
Jarryd Beck <jarro.2783@gmail.com>
2016-12-12 08:14:38 +1100

    add exception to run

--- src/run.py
+++ src/run.py
@@ -5,5 +5,15 @@ This is an executable that runs the pygamelaunch launcher.
 """
 
 import launcher
+import sys
+import traceback
 
-launcher.main()
+try:
+    launcher.main()
+# pylint: disable=bare-except
+except:
+    traceback.print_exc(file=sys.stderr)
+    print("Oops! It looks like pygamelaunch has died.")
+    print("Please report the above output at " +
+          "github.com/jarro2783/pygamelaunch")
+    
